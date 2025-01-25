e43d19d3a477265ff3951b814de79dba5862a2e3
hiback <5335514+hiback@users.noreply.github.com>
2025-01-25 09:35:03 +0900

    添加api认证

--- cf_worker.js
+++ cf_worker.js
@@ -1,33 +1,66 @@
-const hostlist = { 'api.dandanplay.net': null };
+const hostlist = ['api.dandanplay.net'];
+const allowed_methods = 'GET'
+const allowed_hearders = 'Content-Type'
+const max_age = '86400'
 
-async function handleRequest(request) {
-    const urlObj = new URL(request.url);
-    let url = urlObj.href.replace(urlObj.origin + '/cors/', '').trim();
-    if (0 !== url.indexOf('https://') && 0 === url.indexOf('https:')) {
-        url = url.replace('https:/', 'https://');
-    } else if (0 !== url.indexOf('http://') && 0 === url.indexOf('http:')) {
-        url = url.replace('http:/', 'http://');
-    }
-    let tUrlObj = new URL(url);
-    if (!(tUrlObj.hostname in hostlist)) {
-        return Forbidden(tUrlObj);
-    }
-    let response = await fetch(url, {
-        headers: request.headers,
-        body: request.body,
-        method: request.method,
-    });
-    response = new Response(await response.body, response);
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    return response;
+async function handleRequest(request, env) {
+  if (request.method === 'OPTIONS') {
+    return handleOptionsRequest()
+  } else if (request.method === 'GET') {
+    return handleGetRequest(request, env)
+  } else {
+    return Forbidden(`Method ${request.method}`)
+  }
 }
 
-function Forbidden(url) {
-    return new Response(`Hostname ${url.hostname} not allowed.`, {
-        status: 403,
-    });
+async function handleOptionsRequest() {
+  return new Response(null, {
+    status: 204,
+    statusText: 'No Context',
+    headers: new Headers({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': allowed_methods,
+      'Access-Control-Allow-Headers': allowed_hearders,
+      'Access-Control-Max-Age': max_age
+    })
+  })
 }
 
-addEventListener('fetch', (event) => {
-    return event.respondWith(handleRequest(event.request));
-});
+async function handleGetRequest(request, env) {
+  const urlObj = new URL(request.url);
+  let url = urlObj.href.replace(urlObj.origin + '/', '').trim();
+  let tUrlObj
+  try {
+    tUrlObj = new URL(url);
+  }
+  catch {
+    return Forbidden(url)
+  }
+  if (!hostlist.includes(tUrlObj.hostname)) {
+    return Forbidden(tUrlObj);
+  }
+  const new_headers = new Headers(request.headers)
+  new_headers.set('X-Auth', '1')
+  new_headers.set('X-AppId', env.APPID)
+  new_headers.set('X-AppSecret', env.APPSECRET)
+  let response = await fetch(url, {
+    headers: new_headers,
+    body: request.body,
+    method: request.method,
+  });
+  response = new Response(response.body, response);
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  return response;
+}
+
+function Forbidden(str) {
+  return new Response(`${str} Not Allowed.`, {
+    status: 403,
+  });
+}
+
+export default {
+  async fetch(request, env, _) {
+    return handleRequest(request, env);
+  }
+};
